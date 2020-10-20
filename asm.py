bd86f24140607fda89f0d381524c1a70cbc3bedc
mlflanagan <72321981+mlflanagan@users.noreply.github.com>
2020-10-20 10:47:49 -0400

    Update asm.py

--- asm.py
+++ asm.py
@@ -1,101 +1,120 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import os.path
-from typing import List
-
 """
+asm.py - assemble a subleq program into an executable file for subleq.py.
+mlf 2020-10-20
+
 TODO: input/output
 """
 
+import sys
+from typing import List
+
 
 def is_int(s: str) -> bool:
+    """
+    is_int: Check to see if parameter s is an int.
+    :param s: string that may be an int.
+    :return: True if s is integer, False if not
+    """
+    retval = True
     try:
         int(s)
-        return True
     except ValueError:
-        return False
+        retval = False
+    return retval
 
 
 class Assembler:
-    def __init__(self, program):
-        self.program = program
-        self.symbol_table = {}
-        self.tokens = []
-        self.prev_token = ""
-        self.intermediate_output = []
-        self.memory_location = 0
-        self.parse()
+    def __init__(self, program: List[str]):
+        self.program: List[str] = program
+        self.symbol_table: dict = {}
+        self.tokens: List[str] = []
+        self.prev_token: str = ""
+        self.intermediate_output: List[List[str]] = []
+        self.memory_location: int = 0
+        self.assembled_code: List[str] = self.parse()
+
+    def unget_token(self, token: str) -> None:
+        """
+        unget_token: store current token for next step in parsing
+        :param token: current token
+        :return: None
+        """
+        self.prev_token = token
 
     def get_token(self) -> str:
-        try:
-            token = next(self.tokens)
-        except StopIteration:
-            token = ""
+        """
+        get_token: If there is a value in prev_token, return it, otherwise
+        return the next token in the stream.  Return empty string if at end
+        of stream.
+        :return: token
+        """
+        if self.prev_token:
+            token = self.prev_token
+            self.prev_token = ""
+        else:
+            try:
+                token = next(self.tokens)
+            except StopIteration:
+                token = ""
         return token
 
     def parse_line(self, line: str) -> List[str]:
         """
-        minuend: The quantity from which another quantity is to be subtracted.
-        subtrahend: A quantity to be subtracted from another quantity.
-        Example: subleq a b ; b = b - a
-            b is the minuend
-            a is the subtrahend
-        So in subleq logic, first arg = subtrahend and 2nd arg = minuend.
+        parse_line: parse a single line of subleq source code.
+        Called in the first pass.
+        :param line: a string representing the line of subleq
+        source code to parse.
+        :return: parsed_line - a list of strings representing
+        the intermediate code.
         """
-        parsed_line = []
+        parsed_line: List[str] = []
         self.tokens = iter(line.split())
         state = 'START'
         while state != 'HALT':
             if state == 'START':
                 token = self.get_token()
-                # this should never happen
-                # if token[0] == ';':
-                #     # embedded comment, ignore rest of line
-                #     state = 'HALT'
                 if token[-1] == ':':
-                    # label, e.g.:
-                    # Z: 0 (data definition) or
-                    # L1: a b (target of goto)
+                    # we have either a data definition, e.g., Z: 0
+                    # or a goto target, e.g., L1: a b
                     label = token[:-1]
-                    # add label and address to symbol table
                     self.symbol_table[label] = self.symbol_table.get(
                         label,
                         self.memory_location)
-                    state = 'GET-SUBTRAHEND'
+                    state = 'GET-1ST-ARG'
                 elif token == "subleq":
-                    # ignore "subleq" word
-                    state = 'GET-SUBTRAHEND'
+                    # ignore "subleq" keyword
+                    state = 'GET-1ST-ARG'
                 else:
-                    # this is subtrahend
-                    parsed_line.append(token)
-                    self.memory_location += 1
-                    state = 'GET-MINUEND'
-            elif state == 'GET-SUBTRAHEND':
-                # get first arg
+                    # this must the first arg
+                    self.unget_token(token)
+                    state = 'GET-1ST-ARG'
+            elif state == 'GET-1ST-ARG':
+                # the "a" in an instruction like subleq a b addr
                 token = self.get_token()
                 if token == "" or token[0] == ';':
                     raise ValueError(f"need at least one arg: {self.tokens}")
                 else:
                     parsed_line.append(token)
                     self.memory_location += 1
-                    state = 'GET-MINUEND'
-            elif state == 'GET-MINUEND':
-                # get 2nd arg
+                    state = 'GET-2ND-ARG'
+            elif state == 'GET-2ND-ARG':
+                # the "b" in an instruction like subleq a b addr
                 token = self.get_token()
                 if token == "" or token[0] == ';':
                     state = 'HALT'
                 else:
                     parsed_line.append(token)
                     self.memory_location += 1
-                    state = 'GET-ADDRESS'
-            elif state == 'GET-ADDRESS':
-                # get 3rd arg
+                    state = 'GET-3RD-ARG'
+            elif state == 'GET-3RD-ARG':
+                # the address to jump to if b - a <= 0
                 token = self.get_token()
                 if token == "" or token[0] == ';':
-                    # end of line, make implicit address explicit to next line
-                    parsed_line.append(self.memory_location + 1)
+                    # reached eol, implied address is next memory location
+                    parsed_line.append(str(self.memory_location + 1))
                     self.memory_location += 1
                     state = 'HALT'
                 else:
@@ -105,10 +124,13 @@ class Assembler:
                     state = 'HALT'
             else:
                 raise ValueError(f"unknown state: {state}")
-
         return parsed_line
 
-    def parse(self) -> None:
+    def parse(self) -> List[str]:
+        """
+        parse: parse a file of subleq source code
+        :return: list of ints representing the assembled code
+        """
         # first pass - build symbol table
         for line in self.program:
             line = line.strip()
@@ -116,32 +138,39 @@ class Assembler:
                 continue
             parsed_line = self.parse_line(line)
             self.intermediate_output.append(parsed_line)
-        print(f"symbol table = {self.symbol_table}")
-        print(f"intermediate output: {self.intermediate_output}")
 
         # second pass - replace symbols with defined memory locations
-        final_output: list = []
+        final_output: List[str] = []
         for line in self.intermediate_output:
             for symbol in line:
                 if is_int(symbol):
-                    final_output.append(int(symbol))
+                    final_output.append(symbol)
                 else:
                     if symbol in self.symbol_table:
-                        final_output.append(self.symbol_table[symbol])
+                        final_output.append(str(self.symbol_table[symbol]))
                     else:
                         raise ValueError(f"undefined symbol: {symbol}")
 
-        print(f"final output: {final_output}")
+        return final_output
 
 
-def main(args: list) -> None:
-    with open(args[0], 'r') as file:
-        Assembler(file.readlines())
+def main():
+    try:
+        filename = sys.argv[1]
+        name, ext = filename.split(".")
+        with open(filename, 'r') as inf, open(name + ".asm", 'w') as outf:
+            assembler: Assembler = Assembler(inf.readlines())
+            # create a space separated string for writelines
+            output = [str(i) for i in assembler.assembled_code]
+            outf.writelines(' '.join(output))
+    except IndexError:
+        basename = sys.argv[0].split("/")[-1]
+        print(f"usage: {basename} filename")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"can't open file: {sys.argv[1]}")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    try:
-        main(sys.argv[1:])
-    except IndexError:
-        print(f"usage: {os.path.basename(sys.argv[0])} filename")
-        sys.exit(1)
+    main()
